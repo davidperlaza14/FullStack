86f5bd2608ee20012517aa14e8a19f9ff40714d9
davidperlaza14 <davidperlaza14@gmail.com>
2023-02-09 10:09:27 -0500

    ejercicio1.

--- Back-End/Back-End_Con_Python/Python-curso/8_Entrada_Salida/Ejercicios/ejercicio1.py
+++ Back-End/Back-End_Con_Python/Python-curso/8_Entrada_Salida/Ejercicios/ejercicio1.py
@@ -2,7 +2,19 @@
 
 En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado."""
 
-f = open('ejercicio1.txt', 'w')
-lista = ['Hola, mundo\n', 'hola, mundo']
-f.writelines(lista)
+# f = open('ejercicio1.txt', 'w')
+# lista = ['Hola, mundo\n', 'hola, mundo']
+# f.writelines(lista)
+# f.close()
+
+f = open('mi_primer_archivo.txt', 'w')
+f.write('¡He creado mi primer archivo!\n')
+f.close()
+
+f = open('mi_primer_archivo.txt', 'r+')
+f.readline()
+f.write('Esta es la segunda vez que escribo.\n')
+
+f.seek(0)
+print(f.read())
 f.close()
